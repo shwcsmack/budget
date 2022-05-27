e30877115e9997673d03636881cbfb7e7c0f7d22
shwcsmack <sh.wc.smack@gmail.com>
2022-05-27 02:56:51 +0000

    bring in dialoguer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+
 fn main() {
-    println!("Hello World!");
+    print!("Welcome to ZBudget!");
+
+    let selections = &[
+        "New Budget",
+        "Open Budget",
+        "Exit",
+    ];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("How would you like to start?")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    println!("Enjoy your {}!", selections[selection]);
 }
